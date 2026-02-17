b6cf314e9d3252a95c8e44aeae221255b9af5d25
Glauco <37829079+rglauco@users.noreply.github.com>
2026-02-17 21:40:59 +0100

    Align federation metadata models with OpenID Federation 1.0

--- src/main/java/org/keycloak/protocol/oidc/federation/common/beans/FederationEntity.java
+++ src/main/java/org/keycloak/protocol/oidc/federation/common/beans/FederationEntity.java
@@ -2,12 +2,24 @@ package org.keycloak.protocol.oidc.federation.common.beans;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class FederationEntity {
     
-    @JsonProperty("federation_api_endpoint")
-    private String federationApiEndpoint;
+    @JsonProperty("federation_fetch_endpoint")
+    @JsonAlias("federation_api_endpoint")
+    private String federationFetchEndpoint;
+    @JsonProperty("federation_list_endpoint")
+    private String federationListEndpoint;
+    @JsonProperty("federation_resolve_endpoint")
+    private String federationResolveEndpoint;
+    @JsonProperty("federation_trust_mark_status_endpoint")
+    private String federationTrustMarkStatusEndpoint;
+    @JsonProperty("federation_trust_mark_list_endpoint")
+    private String federationTrustMarkListEndpoint;
+    @JsonProperty("federation_trust_mark_endpoint")
+    private String federationTrustMarkEndpoint;
     private String name;
     private List<String> contacts;
     @JsonProperty("policy_uri")
@@ -15,13 +27,66 @@ public class FederationEntity {
     @JsonProperty("homepage_uri")
     private String homepageUri;
     @JsonProperty("trust_marks")
-    private String trustMarks;
+    private List<String> trustMarks;
+
+    public String getFederationFetchEndpoint() {
+        return federationFetchEndpoint;
+    }
+
+    public void setFederationFetchEndpoint(String federationFetchEndpoint) {
+        this.federationFetchEndpoint = federationFetchEndpoint;
+    }
+
+
+    // backward compatibility with draft field name
     public String getFederationApiEndpoint() {
-        return federationApiEndpoint;
+        return federationFetchEndpoint;
     }
+
     public void setFederationApiEndpoint(String federationApiEndpoint) {
-        this.federationApiEndpoint = federationApiEndpoint;
+        this.federationFetchEndpoint = federationApiEndpoint;
     }
+
+    public String getFederationListEndpoint() {
+        return federationListEndpoint;
+    }
+
+    public void setFederationListEndpoint(String federationListEndpoint) {
+        this.federationListEndpoint = federationListEndpoint;
+    }
+
+    public String getFederationResolveEndpoint() {
+        return federationResolveEndpoint;
+    }
+
+    public void setFederationResolveEndpoint(String federationResolveEndpoint) {
+        this.federationResolveEndpoint = federationResolveEndpoint;
+    }
+
+    public String getFederationTrustMarkStatusEndpoint() {
+        return federationTrustMarkStatusEndpoint;
+    }
+
+    public void setFederationTrustMarkStatusEndpoint(String federationTrustMarkStatusEndpoint) {
+        this.federationTrustMarkStatusEndpoint = federationTrustMarkStatusEndpoint;
+    }
+
+    public String getFederationTrustMarkListEndpoint() {
+        return federationTrustMarkListEndpoint;
+    }
+
+    public void setFederationTrustMarkListEndpoint(String federationTrustMarkListEndpoint) {
+        this.federationTrustMarkListEndpoint = federationTrustMarkListEndpoint;
+    }
+
+    public String getFederationTrustMarkEndpoint() {
+        return federationTrustMarkEndpoint;
+    }
+
+    public void setFederationTrustMarkEndpoint(String federationTrustMarkEndpoint) {
+        this.federationTrustMarkEndpoint = federationTrustMarkEndpoint;
+    }
+
     public String getName() {
         return name;
     }
@@ -46,10 +111,11 @@ public class FederationEntity {
     public void setHomepageUri(String homepageUri) {
         this.homepageUri = homepageUri;
     }
-    public String getTrustMarks() {
+    public List<String> getTrustMarks() {
         return trustMarks;
     }
-    public void setTrustMarks(String trustMarks) {
+
+    public void setTrustMarks(List<String> trustMarks) {
         this.trustMarks = trustMarks;
     }
 }
